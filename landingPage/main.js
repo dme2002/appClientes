330cf65bc74e7d7251338ddf028c7e46829f495d
Luis Guillen <luisguillenortiz_8@hotmail.com>
2023-04-12 15:07:26 -0600

    navegacion bienvenida

---
+++ landingPage/main.js
@@ -0,0 +1,6 @@
+
+
+function mostrarPaginaBienvenida() {
+    document.getElementById('seccion-Bienvenida').style.display="block";
+    document.getElementById('seccion-landingPage').style.display="none";
+}
