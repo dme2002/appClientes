afd779769a49ad01a92d6fdc5e85110abc831080
Luis Guillen <luisguillenortiz_8@hotmail.com>
2023-04-21 19:43:51 -0600

    obteniendo ubicacion de usuario

--- landingPage/main.js
+++ landingPage/main.js
@@ -88,6 +88,57 @@ function validarRegistro(e) {
 
 }
 
+function mostrarUbicacionActual() {
+       /** @type {HTMLElement}*/
+       var posElt;
+       /** @type {HTMLElement} */
+       var posLinkElt;
+
+    let mapaImagen=document.getElementById('mapa');
+    let contenidoMapa=document.getElementById('contenido-mapa');
+    let botonMostrar=document.getElementById('boton-confirmar');
+    let botonPagar=document.getElementById('boton-pagar');
+    let contenedorLink=document.getElementById('contenedor-link');
+   
+   
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        var link = document.getElementById("myLink");
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        console.log("Latitud: " + latitude + ", Longitud: " + longitude);
+
+        document.getElementById('Pos').innerHTML=`${latitude}, ${longitude}`;
+        document.getElementById('PosLink').innerHTML='Mostrar tu posición en un mapa';
+       
+        
+
+        link.setAttribute("href", `https://maps.google.com/?q=${latitude},${longitude}`);
+        link.setAttribute("target", "_blank");
+
+
+        });
+    } else {
+        console.log("La localización no es soportada por este navegador.");
+    }
+    
+
+    mapaImagen.setAttribute("style", "display:none;");
+    contenidoMapa.setAttribute("style", "display:none;");
+    botonMostrar.setAttribute("style", "display:none;");
+    botonPagar.setAttribute("style", "display:block;");
+    contenedorLink.setAttribute("style", "display:block;");
+    
+    //agregando coordenadas al mapa
+
+    
+
+
+
+
+
+}
 
 
 
@@ -121,6 +172,7 @@ function mostrarPaginaMenu() {
     document.getElementById('seccion-inicioSesion').style.display="none";
     document.getElementById('seccion-Bienvenida').style.display="none";
     document.getElementById('seccion-landingPage').style.display="none";
+    document.getElementById('seccion-ubicacion').style.display="none";
 }
 
 function mostrarPaginaCarrito() {
@@ -171,3 +223,4 @@ function mostrarPaginaFinalizar() {
     document.getElementById('seccion-Bienvenida').style.display="none";
     document.getElementById('seccion-landingPage').style.display="none";
 }
+
